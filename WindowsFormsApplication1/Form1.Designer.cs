41212ec3f1a043139dcfe8d4535aa269129a9cdd
asafbart <asafbart2gmail.com>
2015-08-08 17:33:16 +0300

    now shows friends born each year

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApplication1
 {
-    partial class Form1
+    public partial class Design_Patterns_App
     {
         /// <summary>
         /// Required designer variable.
@@ -17,6 +17,7 @@
             {
                 components.Dispose();
             }
+
             base.Dispose(disposing);
         }
 
@@ -28,50 +29,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.checkBoxAutomaticLogin = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.UserPictureBox = new System.Windows.Forms.PictureBox();
-            this.Friend_list = new System.Windows.Forms.ListBox();
+            this.Friends_year_list = new System.Windows.Forms.ListBox();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.buttonLogin = new System.Windows.Forms.Button();
+            this.NamesPerChosenYear = new System.Windows.Forms.ListBox();
             ((System.ComponentModel.ISupportInitialize)(this.UserPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // checkBoxAutomaticLogin
+            // checkBox1
             // 
-            this.checkBoxAutomaticLogin.AutoSize = true;
-            this.checkBoxAutomaticLogin.Location = new System.Drawing.Point(117, 28);
-            this.checkBoxAutomaticLogin.Name = "checkBoxAutomaticLogin";
-            this.checkBoxAutomaticLogin.Size = new System.Drawing.Size(98, 17);
-            this.checkBoxAutomaticLogin.TabIndex = 0;
-            this.checkBoxAutomaticLogin.Text = "Automatic login";
-            this.checkBoxAutomaticLogin.UseVisualStyleBackColor = true;
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(117, 28);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // UserPictureBox
             // 
-            this.UserPictureBox.Location = new System.Drawing.Point(29, 93);
+            this.UserPictureBox.Location = new System.Drawing.Point(29, 51);
             this.UserPictureBox.Name = "UserPictureBox";
             this.UserPictureBox.Size = new System.Drawing.Size(133, 110);
             this.UserPictureBox.TabIndex = 1;
             this.UserPictureBox.TabStop = false;
             // 
-            // Friend_list
+            // Friends_year_list
             // 
-            this.Friend_list.FormattingEnabled = true;
-            this.Friend_list.Location = new System.Drawing.Point(298, 56);
-            this.Friend_list.Name = "Friend_list";
-            this.Friend_list.Size = new System.Drawing.Size(173, 147);
-            this.Friend_list.TabIndex = 2;
-            this.Friend_list.SelectedIndexChanged += new System.EventHandler(this.friend_list_SelectedIndexChanged);
+            this.Friends_year_list.FormattingEnabled = true;
+            this.Friends_year_list.Location = new System.Drawing.Point(298, 56);
+            this.Friends_year_list.Name = "Friends_year_list";
+            this.Friends_year_list.Size = new System.Drawing.Size(173, 147);
+            this.Friends_year_list.TabIndex = 2;
+            this.Friends_year_list.SelectedIndexChanged += new System.EventHandler(this.Friends_year_list_SelectedIndexChanged);
             // 
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
             this.linkLabel1.Location = new System.Drawing.Point(295, 40);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(218, 13);
+            this.linkLabel1.Size = new System.Drawing.Size(124, 13);
             this.linkLabel1.TabIndex = 4;
             this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "Get friends who congratulate you on birthday";
+            this.linkLabel1.Text = "Get friends Born per year";
             this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // buttonLogin
@@ -82,20 +84,30 @@
             this.buttonLogin.TabIndex = 5;
             this.buttonLogin.Text = "Login";
             this.buttonLogin.UseVisualStyleBackColor = true;
-            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click_1);
             // 
-            // Form1
+            // NamesPerChosenYear
+            // 
+            this.NamesPerChosenYear.FormattingEnabled = true;
+            this.NamesPerChosenYear.Location = new System.Drawing.Point(194, 136);
+            this.NamesPerChosenYear.Name = "NamesPerChosenYear";
+            this.NamesPerChosenYear.Size = new System.Drawing.Size(93, 56);
+            this.NamesPerChosenYear.TabIndex = 6;
+            this.NamesPerChosenYear.SelectedIndexChanged += new System.EventHandler(this.NamesPerChosenYear_SelectedIndexChanged);
+            // 
+            // Design_Patterns_App
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(516, 320);
+            this.Controls.Add(this.NamesPerChosenYear);
             this.Controls.Add(this.buttonLogin);
             this.Controls.Add(this.linkLabel1);
-            this.Controls.Add(this.Friend_list);
+            this.Controls.Add(this.Friends_year_list);
             this.Controls.Add(this.UserPictureBox);
-            this.Controls.Add(this.checkBoxAutomaticLogin);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Design_Patterns_App";
+            this.Text = "Design Patterns Facebook app";
             ((System.ComponentModel.ISupportInitialize)(this.UserPictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -104,11 +116,11 @@
 
         #endregion
 
-        private System.Windows.Forms.CheckBox checkBoxAutomaticLogin;
+        private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.PictureBox UserPictureBox;
-        private System.Windows.Forms.ListBox Friend_list;
+        private System.Windows.Forms.ListBox Friends_year_list;
         private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.ListBox NamesPerChosenYear;
     }
 }
-
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -5,194 +5,150 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Xml.Serialization;
 using System.Windows.Forms;
 using FacebookWrapper.ObjectModel;
 using FacebookWrapper;
-using System.IO;
 
 namespace WindowsFormsApplication1
 {
-    public partial class Form1 : Form
+    public partial class Design_Patterns_App : Form
     {
-        public delegate void LoginButtonActionDelegate();
-        private User m_LoggedInUser;
-        private LoginButtonActionDelegate m_linkAction;
-        private static readonly string sr_SessionFileName = "session.cfg";
-        private FileStream m_AccessTokenFileStream;
-        private XmlSerializer m_AccessTokenXmlSerializer;
-
-        public Form1()
+        public Design_Patterns_App()
         {
-            InitializeComponent();
-            FacebookWrapper.FacebookService.s_CollectionLimit = 10000;
-            m_AccessTokenXmlSerializer = new XmlSerializer(typeof(string));
-            m_linkAction = new LoginButtonActionDelegate(tryLogin);
-
-            // If there is an access token file
-            if (File.Exists(sr_SessionFileName))
-            {
-                string accessToken = loadAccessToken();
-                if (!string.IsNullOrEmpty(accessToken))
-                {
-                    try
-                    {
-                        loginWithLoginResult(FacebookService.Connect(accessToken));
-                        checkBoxAutomaticLogin.Checked = true;
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.Message);
-                    }
-                }
-            }
+            this.InitializeComponent();
+            FacebookWrapper.FacebookService.s_CollectionLimit = 1000;
         }
 
-        private void tryLogin()
+        public User m_LoggedInUser;
+        public Dictionary<int, List<User>> m_FriendsBornPerYear = new Dictionary<int, List<User>>();
+
+        private void loginAndInit()
         {
+            /// Owner: design.patterns
+
             /// Use the FacebookService.Login method to display the login form to any user who wish to use this application.
-            /// You can then save the result.AccessToken for future auto-connect to this user:
-            LoginResult result = FacebookService.Login("511256585691702", /// (Asaf Haim & Asaf Bartov app)
-                                                       "user_about_me", "user_friends", "publish_actions", "user_events", "user_posts", 
-                                                       "user_photos", "user_status", "user_birthday", "user_likes", "rsvp_event", "user_groups");
-
-            // These are NOT the complete list of permissions. Other permissions for example:
-            // "user_birthday", "user_education_history", "user_hometown", "user_likes","user_location","user_relationships","user_relationship_details","user_religion_politics", "user_videos", "user_website", "user_work_history", "email","read_insights","rsvp_event","manage_pages"
-            // The documentation regarding facebook login and permissions can be found here: 
-            // v2.4: https://developers.facebook.com/docs/facebook-login/permissions/v2.4
-
-            loginWithLoginResult(result);
-        }
-
-        private void loginWithLoginResult(LoginResult i_LoginResult)
-        {
-            if (i_LoginResult != null)
+            /// You can then save the result.AccessToken for future auto-connect to this user:       
+            LoginResult result = FacebookService.Login("511256585691702", "user_status", "user_birthday", "user_about_me", "user_friends", "publish_actions", "user_events", "user_posts", "user_photos" );  /// (desig patter's "Design Patterns Course App 2.4" app)
+            ///  These are NOT the complete list of permissions. Other permissions for example:
+            /// "user_birthday", "user_education_history", "user_hometown", "user_likes","user_location","user_relationships","user_relationship_details","user_religion_politics", "user_videos", "user_website", "user_work_history", "email","read_insights","rsvp_event","manage_pages"
+            /// The documentation regarding facebook login and permissions can be found here: 
+            /// v2.4: https://developers.facebook.com/docs/facebook-login/permissions/v2.4
+            if (!string.IsNullOrEmpty(result.AccessToken))
             {
-                if (!string.IsNullOrEmpty(i_LoginResult.AccessToken))
-                {
-                    if (checkBoxAutomaticLogin.Checked)
-                    {
-                        saveAccessToken(i_LoginResult.AccessToken);
-                    }
-
-                    checkBoxAutomaticLogin.Visible = false;
-                    m_LoggedInUser = i_LoginResult.LoggedInUser;
-                    fetchUserInfo();
-                    switchToLogoutButton();
-                }
-                else
-                {
-                    MessageBox.Show(i_LoginResult.ErrorMessage);
-                }
+                this.m_LoggedInUser = result.LoggedInUser;
+                this.fetchUserInfo();
             }
-        }
-
-        private void saveAccessToken(string i_AccessToken)
-        {
-            using (m_AccessTokenFileStream = new FileStream(sr_SessionFileName, FileMode.OpenOrCreate))
+            else
             {
-                m_AccessTokenXmlSerializer.Serialize(m_AccessTokenFileStream, i_AccessToken);
+                MessageBox.Show(result.ErrorMessage);
             }
-        }
-
-        private string loadAccessToken()
-        {
-            string accesToken = "";
-
-            try
-            {
-                using (m_AccessTokenFileStream = new FileStream(sr_SessionFileName, FileMode.OpenOrCreate))
-                {
-                    accesToken = m_AccessTokenXmlSerializer.Deserialize(m_AccessTokenFileStream) as string;
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
-            return accesToken;
-        }
-
-        private void logout()
-        {
-            clearUserData();
-            FacebookService.Logout(null);
-            checkBoxAutomaticLogin.Visible = true;
-            switchToLoginButton();
-            File.Delete(sr_SessionFileName);
-        }
-
-        private void clearUserData()
-        {
-            UserPictureBox.Image = null;
         }
 
         private void fetchUserInfo()
         {
-            UserPictureBox.LoadAsync(m_LoggedInUser.PictureNormalURL);
-            /*if (m_LoggedInUser.Statuses.Count > 0)
-            {
-                textBoxStatus.Text = m_LoggedInUser.Statuses[0].Message; 
-            }*/
+            this.UserPictureBox.LoadAsync(this.m_LoggedInUser.PictureNormalURL);
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-
         }
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            FacebookObjectCollection<User> myFriends = m_LoggedInUser.Friends;
-            foreach (User friend in myFriends)
+            Dictionary<int, List<User>> o_FriendsBornPerYear = new Dictionary<int, List<User>>();
+            int friendYearBorn;
+            FacebookObjectCollection<User> myFriends = this.m_LoggedInUser.Friends;
+            if (myFriends.Count == 0)
             {
-                int friendSimilarityNum = 0;
-                try
+                MessageBox.Show("No Friends Birthday's to retrieve :(");
+            }
+            else
+            {
+                foreach (var friend in myFriends)
                 {
-                    FacebookObjectCollection<Group> friendGroups = friend.Groups;
-                FacebookObjectCollection<Group> myGroups = m_LoggedInUser.Groups;
-                foreach (Group myGroup in myGroups)
-                {
-                    foreach (Group friendGroup in friendGroups)
+                    friendYearBorn = this.getYear(friend.Birthday);
+                    if (friendYearBorn != 0)
                     {
-                        if (friendGroup == myGroup)
+                        if (o_FriendsBornPerYear.ContainsKey(friendYearBorn) == true)
                         {
-                            friendSimilarityNum++;
+                            o_FriendsBornPerYear[friendYearBorn].Add(friend);
+                        }
+                        else
+                        {
+                            List<User> initialList = new List<User>();
+                            initialList.Add(friend);
+                            o_FriendsBornPerYear.Add(friendYearBorn, initialList);
                         }
                     }
+
+                    this.m_FriendsBornPerYear = o_FriendsBornPerYear;
                 }
-                string friendSimilarity = friend.FirstName;
-                friendSimilarity += friendSimilarityNum.ToString();
-                Friend_list.Items.Add(friendSimilarity);
-                }
-                catch (Exception exs)
+                if (o_FriendsBornPerYear.Count != 0)
                 {
-                  
+                    this.Friend_list_SelectedIndexChanged();
+                }
+                else
+                {
+                    MessageBox.Show("No Friends Birthday's to retrieve :(");
                 }
             }
         }
 
-        private void friend_list_SelectedIndexChanged(object sender, EventArgs e)
+        private int getYear(string i_Birthday)
         {
+            int o_year = 0;
+            char[] delimiterChars = { ' ', ',', '.', ':', '\t', '/' };
+            string[] words = i_Birthday.Split(delimiterChars);
+            if (words.Length == 3)
+            {
+                o_year = int.Parse(words[2]);
+            }
 
+            return o_year;
         }
 
-        private void buttonLogin_Click(object sender, EventArgs e)
+        private void Friend_list_SelectedIndexChanged()
         {
-            m_linkAction();
+            foreach (KeyValuePair<int, List<User>> entry in this.m_FriendsBornPerYear)
+            {
+                this.Friends_year_list.Items.Add(entry.Key.ToString());
+            }
         }
 
-        private void switchToLoginButton()
+        private void displaySelectedFriendsInYear(int i_year)
         {
-            m_linkAction = tryLogin;
-            buttonLogin.Text = "Login";
+            if (Friends_year_list.SelectedItems.Count == 1)
+             {
+                 this.NamesPerChosenYear.DisplayMember = "Name";
+                 List<User> selectedYearFriends = this.m_FriendsBornPerYear[i_year] as List<User>;
+                 foreach (var friend in selectedYearFriends)
+                 {
+                     this.NamesPerChosenYear.Items.Add(friend);
+                 }
+             }
         }
 
-        private void switchToLogoutButton()
+        private void GetCongtatulatingFriends()
         {
-            m_linkAction = logout;
-            buttonLogin.Text = "Logout";
+            List<string> congratulatingFriends = new List<string>();
+        }
+
+        private void buttonLogin_Click_1(object sender, EventArgs e)
+        {
+            this.loginAndInit();
+        }
+
+        private void NamesPerChosenYear_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void Friends_year_list_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (Friends_year_list.SelectedItems.Count == 1)
+            {
+                int selectedYear = int.Parse(Friends_year_list.SelectedItem.ToString());
+                this.displaySelectedFriendsInYear(selectedYear);
+            }
         }
     }
 }
